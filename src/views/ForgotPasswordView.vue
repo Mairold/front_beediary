6981ebf25d3f32fcd5d4e5a7fbaec792c5cbd223
Heda <hedakalmus@gmail.com>
2023-02-22 18:35:10 +0200

    tagasi nupp forgot password views

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -5,8 +5,13 @@
       <input v-if="isView" v-model="email" type="email" class="form-control" aria-describedby="emailHelp">
       <div v-if="isView" id="emailHelp" class="form-text">Sisesta oma e-maili aadress ning saadame sulle uue parooli.
       </div>
-      <button v-if="isView" v-on:click="sendNewPassword" type="button" class="btn btn-warning my-3">Saada uus parool
-      </button>
+      <div>
+        <button v-if="isView" v-on:click="sendNewPassword" type="button" class="btn btn-warning my-3">Saada uus parool
+        </button>
+      </div>
+      <div>
+        <button v-on:click="$router.go(-1)" type="button" class="btn btn-back">Tagasi</button>
+      </div>
       <h4 v-if="isSent">Uus parool on sinu emailile saadetud!</h4>
     </div>
   </div>

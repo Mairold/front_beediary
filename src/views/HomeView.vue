d5695b6afc4a28bb46af8d39d5f06dc245a9992c
Heda <hedakalmus@gmail.com>
2023-02-23 10:16:21 +0200

    väiksed muutused

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
   <div>
     <div>
       <br>
-      <h3>Tere tulemast!</h3>
+      <h2>Tere tulemast!</h2>
       <img alt="Bee logo" src="../assets/bee.gif" height="250">
       <div>
-        <h6 class="my-3">Digiajastu mesinduspäevik hoiab kõik sinu mesilate ja tarude info ühes kohas.</h6>
-        <button v-on:click="navigateToLogin" type="button" class="btn btn-warning">Logi sisse</button>
+        <h6 class="my-4">Digiajastu mesinduspäevik hoiab kõik sinu mesilate ja tarude info ühes kohas.</h6>
+        <button v-on:click="navigateToLogin" type="button" class="btn btn-warning my-3">Logi sisse</button>
       </div>
     </div>
   </div>
